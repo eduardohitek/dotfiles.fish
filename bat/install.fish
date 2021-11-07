d3c9828ee1c483c27f0e59983978d5b9713c11c5
Eduardo Hitek <eduardohitek@gmail.com>
2021-11-07 17:27:32 +0100

    feat: ajustes para usar no linux

--- bat/install.fish
+++ bat/install.fish
@@ -3,3 +3,9 @@ if command -qa bat
 	abbr -a cat 'bat'
 	set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 end
+
+if command -qa batcat
+	abbr -a cat 'batcat'
+    abbr -a bat 'batcat'
+	set -Ux MANPAGER "sh -c 'col -bx | batcat -l man -p'"
+end
